5b26a714d0e6aae2c19ce666e86b2bc528e71906
Dark Young <16302010059@fudan.edu.cn>
2018-12-19 21:20:43 +0800

    build project

---
+++ project_3/cmake-build-debug/CMakeFiles/cjpeg.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/src/cio.c" "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/cmake-build-debug/CMakeFiles/cjpeg.dir/src/cio.c.o"
+  "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/src/cjpeg.c" "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/cmake-build-debug/CMakeFiles/cjpeg.dir/src/cjpeg.c.o"
+  "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/src/cmarker.c" "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/cmake-build-debug/CMakeFiles/cjpeg.dir/src/cmarker.c.o"
+  "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/src/rdbmp.c" "/home/darkyoung/Learning/matlab/Fundamentals-of-Multimedia/project_3/cmake-build-debug/CMakeFiles/cjpeg.dir/src/rdbmp.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project_3/cmake-build-debug/CMakeFiles/cjpeg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cjpeg.dir/src/cjpeg.c.o"
+  "CMakeFiles/cjpeg.dir/src/cio.c.o"
+  "CMakeFiles/cjpeg.dir/src/cmarker.c.o"
+  "CMakeFiles/cjpeg.dir/src/rdbmp.c.o"
+  "cjpeg.pdb"
+  "cjpeg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cjpeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
